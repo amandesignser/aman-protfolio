9d6a0d58dd1c4146cf17921b2bb34a9cbd464fac
Aman Kumar <amanbarnd@gmail.com>
2026-02-19 21:54:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+//   base: '/aman-protfolio/',
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/aman-protfolio/',
+  plugins: [react()],
+  base: '/aman-protfolio/', // 👈 IMPORTANT
 })
