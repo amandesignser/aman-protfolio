e182cb5b4be3b3cd70e70b42ecf43c31d192c344
Aman Kumar <amanbarnd@gmail.com>
2026-02-19 11:36:03 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-red-500'>
-        Aman Kumar
-      </h1>
-    </div>
+    <>
+    <nav/>
+    </>
   )
 }
 
---
+++ src/file/nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li className='text-white'>Home</li>
+            <li className='text-white'>About</li>
+            <li className='text-white'>Contact</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default nav
