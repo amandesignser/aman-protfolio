a703865155dde07fa401741adae59eef2c7b3e36
Aman Kumar <amanbarnd@gmail.com>
2026-02-19 21:59:30 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <>
-    <nav/>
+    <div className="w-screen h-screen bg-black p-2.5">
+      <div className="border border-white w-full h-20 rounded-lg items-center text-center justify-center flex">
+        <span className='text-red-500 text-6xl font-bold'>AMANDESIGNSER</span>
+      </div>
+    </div>
     </>
   )
 }
